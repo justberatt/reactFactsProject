71bc72627f87d7012d606a9a1819d54f926c34a5
Berat Sakipi <berat.sakipi@gmail.com>
2024-12-26 00:20:49 +0100

    A simple react layout just to test my newly acquired skills

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <div className="container">
+  <header>
+      <img src="src/assets/react-logo.png" className='logo' alt="React logo"></img>
+  </header>
+  <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+          <li>Was released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+  </main>
+</div>
 )
