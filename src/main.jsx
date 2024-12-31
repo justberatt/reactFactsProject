c5bf40e3d2d604a1829ee8d11f1b86dca33f6055
Berat Sakipi <berat.sakipi@gmail.com>
2025-01-01 00:10:13 +0100

    reorganized components

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,8 @@
 import { createRoot } from 'react-dom/client'
-
+import Header from '../src/components/Header'
+import MainContent from '../src/components/MainContent'
+import Footer from '../src/components/Footer'
 const root = createRoot(document.getElementById('root'))
-
-function Header() {
-  return (
-      <header>
-          <img src="src/assets/react-logo.png" width="40px" alt="React logo" />
-      </header>
-  )
-}
-
-function MainContent() {
-  return (
-      <main>
-          <h1>Reason I am excited to learn React</h1>
-          <ol>
-              <li>React is a  popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-              <li>I am more likely to get a job as a front end developer if I know React</li>
-          </ol>
-      </main>
-  )
-}
-
-function Footer() {
-  return (
-      <footer>
-          <small>© 2024 Ziroll development. All rights reserved.</small>
-      </footer>
-  )
-}
 
 function Page() {
     return (
@@ -36,7 +10,7 @@ function Page() {
         <Header />
         <MainContent />
         <Footer />
-    </>
+      </>
     )
 }
 
