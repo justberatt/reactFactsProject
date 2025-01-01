eb5fe2b76f1dfdc9a3168c33b281380c4ae9e3fa
Berat Sakipi <berat.sakipi@gmail.com>
2025-01-02 00:42:43 +0100

    finishing touches

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="footer">
-            <small>© 2024 Ziroll development. All rights reserved.</small>
+            <small>© 2024 Sakipi development. All rights reserved.</small>
         </footer>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import Header from '../src/components/Header'
 import MainContent from '../src/components/MainContent'
 import Footer from '../src/components/Footer'
+
 const root = createRoot(document.getElementById('root'))
 
 function Page() {
